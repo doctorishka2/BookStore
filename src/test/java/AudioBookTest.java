ae66f53a86e91945641a5ea07ed6c3690e166ac4
g67oleg@gmail.com <kharkov67>
2024-05-30 23:09:53 -0400

    Commit one file

--- src/test/java/AudioBookTest.java
+++ src/test/java/AudioBookTest.java
@@ -3,6 +3,8 @@ public class AudioBookTest {
     public static void main(String[] args) {
 
         System.out.println("BookStore");
+        System.out.println("BookStore");
+        System.out.println("BookStore");
 
     }
 
